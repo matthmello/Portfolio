ffdc24d11584afb3fdd4f2e854679aa608a6fb13
Matheus Melo <matheus_lpo@hotmail.com>
2025-03-29 17:08:46 -0300

    Social link broken Fixed

--- src/components/SocialIcons/index.tsx
+++ src/components/SocialIcons/index.tsx
@@ -7,7 +7,7 @@ export default function SocialIcons () {
             <Link href="https://github.com/matthmello" target='_blank'>
                 <GitHub/> 
             </Link>
-            <Link href="https://www.linkedin.com/in/matheus-carneiro-melo-784b4b240/" target='_blank'>
+            <Link href="https://www.linkedin.com/in/matheus-melo-784b4b240/" target='_blank'>
                 <LinkedIn/>
             </Link>
             <Link href="https://www.instagram.com/matt_mello_/" target='_blank'>
